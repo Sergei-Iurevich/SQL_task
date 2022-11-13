04d7a4571ed4628111c0ee6f817410f246a54250
Sergei Iurevich <lask2su@gmail.com>
2022-11-13 16:10:13 +0300

    Add subtask

--- task_1.1.sql
+++ task_1.1.sql
@@ -1,6 +1,13 @@
+-- 1. Все товары, в которых в название есть пометка urgent или название начинается с Animal
+
 SELECT *
 from Warehouse.StockItems 
 where StockItemName like '%urgent%' or StockItemName like 'Animal%'
 
+--2. Поставщиков, у которых не было сделано ни одного заказа (потом покажем как это делать через подзапрос, сейчас сделайте через JOIN)
 
-
+select sup.SupplierID, sup.SupplierName, ord.PurchaseOrderID
+from Purchasing.Suppliers as sup
+LEFT JOIN Purchasing.PurchaseOrders as ord 
+on (ord.SupplierID=sup.SupplierID)
+WHERE ord.PurchaseOrderID is NULL
