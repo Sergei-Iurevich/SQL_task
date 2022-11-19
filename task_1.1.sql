8442faaf0d1cb7bf4d3a2f9edb4fa942eb8b84c1
Sergei Iurevich <lask2su@gmail.com>
2022-11-19 13:15:11 +0300

    add new subtask

--- task_1.1.sql
+++ task_1.1.sql
@@ -11,3 +11,31 @@ from Purchasing.Suppliers as sup
 LEFT JOIN Purchasing.PurchaseOrders as ord 
 on (ord.SupplierID=sup.SupplierID)
 WHERE ord.PurchaseOrderID is NULL
+
+--3. Продажи с названием месяца, в котором была продажа, номером квартала, к которому относится продажа, включите также к какой трети года относится дата - каждая треть по 4 месяца, дата забора заказа должна быть задана, с ценой товара более 100$ либо количество единиц товара более 20. Добавьте вариант этого запроса с постраничной выборкой пропустив первую 1000 и отобразив следующие 100 записей. Соритровка должна быть по номеру квартала, трети года, дате продажи.
+
+select ord.OrderID,ord.OrderDate, DATENAME(MONTH,ord.OrderDate) as month,DATENAME(QUARTER,ord.OrderDate) as quarter,
+case 
+    when Month(ord.OrderDate)<=4 then '1 third'
+    when Month(ord.OrderDate)<=8 then '2 third'
+    else '3 third'
+end as third,
+ord.ExpectedDeliveryDate,
+ol.UnitPrice
+from Sales.Orders as ord
+INNER JOIN Sales.OrderLines as ol on (ol.OrderID=ord.OrderID)
+WHERE (ol.UnitPrice>100 or ol.Quantity>20)
+
+select DATENAME(MONTH,ord.OrderDate) as month,DATENAME(QUARTER,ord.OrderDate) as quarter,
+case 
+    when Month(ord.OrderDate)<=4 then '1 third'
+    when Month(ord.OrderDate)<=8 then '2 third'
+    else '3 third'
+end as third,
+ord.ExpectedDeliveryDate
+from Sales.Orders as ord
+INNER JOIN Sales.OrderLines as ol on (ol.OrderID=ord.OrderID)
+WHERE (ol.UnitPrice>100 or ol.Quantity>20)
+order by quarter,third, ord.OrderDate
+OFFSET 1000 ROWS FETCH NEXT 100 ROWS ONLY
+
